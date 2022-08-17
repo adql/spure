e30d4296aa7aece5168e37244728adde043a2192
Amir Dekel <6893125+adql@users.noreply.github.com>
2022-08-17 21:03:16 +0200

    Use modern React renderRoot

--- src/Main.purs
+++ src/Main.purs
@@ -5,8 +5,8 @@ import Prelude
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (error)
+import React.Basic.DOM.Client (createRoot, renderRoot)
 import Spure (mkSpure)
-import React.Basic.DOM (render)
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
@@ -15,9 +15,10 @@ import Web.HTML.Window (document)
 main :: Effect Unit
 main = do
   doc <- document =<< window
-  app <- getElementById "app" $ toNonElementParentNode doc
-  case app of
+  app' <- getElementById "app" $ toNonElementParentNode doc
+  case app' of
     Nothing -> error "No app element found"
-    Just app' -> do
+    Just app -> do
+      root <- createRoot app
       spure <- mkSpure
-      render (spure unit) app'
+      renderRoot root $ spure unit
