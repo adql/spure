863627d6682b1846a119ed4ade64c186400131af
Amir Dekel <6893125+adql@users.noreply.github.com>
2022-08-20 11:12:36 +0200

    Add state of text and input deletion on line break

--- src/Spure.purs
+++ src/Spure.purs
@@ -3,7 +3,7 @@ module Spure (mkSpure)
 
 import Prelude
 
-import Data.Array (elem)
+import Data.Array (elem, snoc)
 import Data.Maybe (Maybe(..), fromJust)
 import Data.Nullable (null)
 import Effect (Effect)
@@ -11,39 +11,58 @@ import Partial.Unsafe (unsafePartial)
 import React.Basic.DOM as D
 import React.Basic.DOM.Events (capture, capture_, key, nativeEvent, target)
 import React.Basic.Events (handler, merge)
-import React.Basic.Hooks (Component, component)
+import React.Basic.Hooks (Component, Reducer, component, mkReducer, readRefMaybe, useEffectOnce, useReducer, useRef, (/\))
 import React.Basic.Hooks as R
 import Spure.Internal.InputEvent (inputType)
-import Web.DOM.Node (toEventTarget)
+import Web.DOM.Node (Node, toEventTarget)
 import Web.Event.Event (Event, preventDefault)
 import Web.Event.EventTarget (EventTarget, addEventListener, eventListener)
 import Web.HTML.HTMLElement (focus, fromEventTarget)
+import Web.HTML.HTMLInputElement as HtmlIE
 import Web.UIEvent.InputEvent (fromEvent)
 import Web.UIEvent.InputEvent.EventTypes (beforeinput)
 
-mkSpure :: Component Unit
-mkSpure = component "Spure" \_ -> R.do
-  spureRef <- R.useRef null
+mkTextReducer :: Effect (Reducer (Array String) String)
+mkTextReducer = mkReducer snoc
 
-  R.useEffectOnce do
-    spure <- R.readRefMaybe spureRef
-    case spure of
-      Nothing -> pure mempty
-      Just node -> do
-        let eTarget = toEventTarget node
-        addInputBlocker eTarget
-        pure mempty
-  
-  pure $ D.div { id:"container"
-               , children: [ D.input { ref: spureRef
-                                     , id: "spure"
-                                     , spellCheck: false
-                                     , onKeyDown: handler (merge { key, nativeEvent }) handleKeyDown
-                                     , onMouseDown: capture target handleMouseDown
-                                     , onContextMenu: capture_ $ pure unit
-                                     }
-                           ]
-               }
+mkSpure :: Component Unit
+mkSpure = mkReducer snoc >>= \textReducer ->
+  component "Spure" \_ -> R.do
+    text /\ dispatch <- useReducer [] textReducer
+    spureRef <- useRef null
+
+    let beforeInputHandler :: Node -> Effect Unit
+        beforeInputHandler node =
+          let eTarget = toEventTarget node
+              inputElem = (unsafePartial fromJust) $ HtmlIE.fromNode node
+          in do
+            eListener <- eventListener \e -> do
+              let inputE = (unsafePartial fromJust) $ fromEvent e
+              case inputType inputE of
+                "insertText" -> pure unit
+                "insertLineBreak" -> do
+                  newParagraph <- HtmlIE.value inputElem
+                  HtmlIE.setValue "" inputElem
+                  dispatch newParagraph
+                otherwise -> preventDefault e
+            addEventListener beforeinput eListener true eTarget
+
+    useEffectOnce do
+      spure <- readRefMaybe spureRef
+      case spure of
+        Nothing -> pure mempty
+        Just node -> beforeInputHandler node *> pure mempty
+    
+    pure $ D.div { id:"container"
+                 , children: [ D.input { ref: spureRef
+                                       , id: "spure"
+                                       , spellCheck: false
+                                       , onKeyDown: handler (merge { key, nativeEvent }) handleKeyDown
+                                       , onMouseDown: capture target handleMouseDown
+                                       , onContextMenu: capture_ $ pure unit
+                                       }
+                             ]
+                 }
 
 forbiddenKeyValues :: Array String
 forbiddenKeyValues = [ "ArrowDown"
@@ -64,18 +83,3 @@ handleKeyDown { key, nativeEvent } =
 
 handleMouseDown :: EventTarget -> Effect Unit
 handleMouseDown = focus <<< (unsafePartial fromJust) <<< fromEventTarget
-
-allowedInputTypes :: Array String
-allowedInputTypes = [ "insertText"
-                    , "insertLineBreak"
-                    , "insertParagraph"
-                    ]
-
-addInputBlocker :: EventTarget -> Effect Unit
-addInputBlocker eTarget = do
-  eListener <- eventListener \e -> do
-    let inputE = (unsafePartial fromJust) $ fromEvent e
-    if inputType inputE `elem` allowedInputTypes
-      then pure unit
-      else preventDefault e
-  addEventListener beforeinput eListener true eTarget
