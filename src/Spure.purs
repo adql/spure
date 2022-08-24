7e54440c36e25cd21693c9a4804e0cc400b36b25
Amir Dekel <6893125+adql@users.noreply.github.com>
2022-08-24 07:30:40 +0200

    Extract controlled input logic to a hook

--- src/Spure.purs
+++ src/Spure.purs
@@ -5,19 +5,21 @@ import Prelude
 
 import Data.Array (elem)
 import Data.Maybe (Maybe(..), fromJust)
-import Data.Nullable (null)
+import Data.Newtype (class Newtype)
+import Data.Nullable (Nullable, null)
 import Data.String as S
 import Effect (Effect)
 import Partial.Unsafe (unsafePartial)
 import React.Basic.DOM as D
 import React.Basic.DOM.Events (capture, capture_, key, nativeEvent, target)
 import React.Basic.Events (handler, merge)
-import React.Basic.Hooks (Component, component, readRefMaybe, useEffectOnce, useRef)
+import React.Basic.Hooks (Component, Hook, Ref, UseEffect, UseRef, coerceHook, component, readRefMaybe, useEffect, useRef)
 import React.Basic.Hooks as R
 import Spure.Internal.InputEvent (inputType)
 import Web.DOM.Node (Node, toEventTarget)
 import Web.Event.Event (Event, preventDefault)
 import Web.Event.EventTarget (EventTarget, addEventListener, eventListener)
+import Web.HTML (HTMLInputElement)
 import Web.HTML.HTMLElement (focus, fromEventTarget)
 import Web.HTML.HTMLInputElement as HtmlIE
 import Web.UIEvent.InputEvent (fromEvent)
@@ -26,31 +28,15 @@ import Web.UIEvent.InputEvent.EventTypes (beforeinput)
 mkSpure :: Component { setText::
                           (Array String -> Array String) -> Effect Unit }
 mkSpure = component "Spure" \{setText} -> R.do
-  spureRef <- useRef null
+  let appendParagraph :: HTMLInputElement -> Effect Unit
+      appendParagraph inputElem = do
+        newParagraph <- HtmlIE.value inputElem
+        HtmlIE.setValue "" inputElem
+        if not (S.null newParagraph) then setText (_ <> [newParagraph])
+          else pure unit
 
-  let beforeInputHandler :: Node -> Effect Unit
-      beforeInputHandler node =
-        let eTarget = toEventTarget node
-            inputElem = (unsafePartial fromJust) $ HtmlIE.fromNode node
-        in do
-          eListener <- eventListener \e -> do
-            let inputE = (unsafePartial fromJust) $ fromEvent e
-            case inputType inputE of
-              "insertText" -> pure unit
-              "insertLineBreak" -> do
-                newParagraph <- HtmlIE.value inputElem
-                HtmlIE.setValue "" inputElem
-                if not (S.null newParagraph) then setText (_ <> [newParagraph])
-                  else pure unit
-              _ -> preventDefault e
-          addEventListener beforeinput eListener true eTarget
+  spureRef <- useControlledInput appendParagraph
 
-  useEffectOnce do
-    spure <- readRefMaybe spureRef
-    case spure of
-      Nothing -> pure mempty
-      Just node -> beforeInputHandler node *> pure mempty
-    
   pure $ D.input { ref: spureRef
                  , id: "spure"
                  , spellCheck: false
@@ -58,6 +44,33 @@ mkSpure = component "Spure" \{setText} -> R.do
                  , onMouseDown: capture target handleMouseDown
                  , onContextMenu: capture_ $ pure unit
                  }
+
+newtype UseControlledInput hooks =
+  UseControlledInput (UseEffect Unit (UseRef (Nullable Node) hooks))
+
+derive instance ntUseControlledInput :: Newtype (UseControlledInput hooks) _
+
+useControlledInput :: (HTMLInputElement -> Effect Unit) ->
+                      Hook UseControlledInput (Ref (Nullable Node))
+useControlledInput handleLineBreak = coerceHook R.do
+  spureRef <- useRef null
+  useEffect unit do
+    spure <- readRefMaybe spureRef
+    case spure of
+      Nothing -> pure mempty
+      Just node -> do
+        let eTarget = toEventTarget node
+            inputElem = (unsafePartial fromJust) $ HtmlIE.fromNode node
+        eListener <- eventListener \e -> do
+          let inputE = (unsafePartial fromJust) $ fromEvent e
+          case inputType inputE of
+            "insertText" -> pure unit
+            "insertLineBreak" -> handleLineBreak inputElem
+            _ -> preventDefault e
+        addEventListener beforeinput eListener true eTarget
+        pure mempty
+
+  pure spureRef
 
 forbiddenKeyValues :: Array String
 forbiddenKeyValues = [ "ArrowDown"
